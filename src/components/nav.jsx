609dca53429dcc0b8acd48c9f26b10252140a681
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-06-29 14:02:58 +0100

    Started build of Emerald homepage clone. Nav complete

---
+++ src/components/nav.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+
+  render() {
+    return(
+        <div className="nav">
+        <img
+          className="logo"
+          src="https://www.achat-cloud.fr/wp-content/uploads/cloud.png"
+          alt="new"
+        />
+        <p className="log-btn">Log in</p>
+        </div>
+
+
+    );
+  };
+}
+
+export default Nav
