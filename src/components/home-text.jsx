84fbc81ddc74e0cc21d9fc672860160f985846b3
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-06-30 22:30:06 +0100

    Made responsive to landscape mobiles

--- src/components/home-text.jsx
+++ src/components/home-text.jsx
@@ -4,12 +4,14 @@ class HomeText extends Component {
 
   render() {
     return(
-      <div className="main-text">
-        <h1>Build safer with <span id="emerald">Emerald</span></h1>
-        <div className="banner-p">
-          <p>Create, monitor and manage your permits to reduce risks to worker safety, delays and to save money across all of your projects.</p>
+      <div className="main-text-wrap">
+        <div className="main-text">
+          <h1>Build safer with <span id="emerald">Emerald</span></h1>
+          <div className="banner-p">
+            <p>Create, monitor and manage your permits to reduce risks to worker safety, delays and to save money across all of your projects.</p>
+          </div>
+          <a href="" className="login-btn log-btn">Sign up</a>
         </div>
-        <a href="" className="login-btn log-btn">Sign up</a>
       </div>
     );
   };
