9696d3f540ab83d29e7f6d02f4cdcf623056d95d
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-06-30 21:59:43 +0100

    Added changing background

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,34 +5,27 @@ import HomeText from './home-text'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { background: 0 };
+    this.state = { background: 1 };
   }
 
-  changeState = setTimeout(() => {
-    this.setState({ background: 1 })
-  }, 2000);
-
-  bgc = () => {
-    let background = [
-        { backgroundImage: "url('https://bit.ly/2YnzrI6')" },
-        { backgroundImage: "url('https://bit.ly/2xmOZQm')" },
-        { backgroundImage: "url('https://bit.ly/2LtwwtI')" }
-      ]
-      if (this.state.background === 0) {
-        return background[0];
-        changeState()
-      }
-      else if (this.state.background === 1) {
-        return background[1];
-        this.setState({ background: 2 })
-      }
-  }
-
-
+  classes = () => {
+    let thisClass = "home-image";
+    if (this.state.background === 1) {
+      thisClass += " img-one";
+      setTimeout(() => {this.setState({ background: 2 })}, 5000);
+    } else if (this.state.background === 2) {
+      thisClass += " img-two";
+      setTimeout(() => {this.setState({ background: 3 })}, 5000);
+    } else if (this.state.background === 3) {
+      thisClass += " img-three";
+      setTimeout(() => {this.setState({ background: 1 })}, 5000);
+    }
+    return thisClass;
+  };
 
   render() {
     return(
-      <div className="home-image" style={ this.bgc() }>
+      <div className={this.classes()}>
         <div className="main">
           <Nav />
           <HomeText />
